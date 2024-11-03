5ca38775fce23bbd5e796a023d20b1e3e12c093f
kyy <134707875+zackyyhw@users.noreply.github.com>
2024-11-03 07:08:14 +0700

    Delete Pertemuan 6/perpustakaanrestful/perpustakaan directory

--- Pertemuan 6/perpustakaanrestful/perpustakaan/src/main/java/com/polstat/perpustakaan/service/BookService.java
+++
@@ -1,8 +0,0 @@
-package com.polstat.perpustakaan.service;
-import com.polstat.perpustakaan.dto.BookDto;
-import java.util.List;
-public interface BookService {
-    public void createBook(BookDto bookDto);
-    public List<BookDto> getBooks();
-    public List<BookDto> searchBooks(String keyword);
-}
